cdbedb663b1557443dc515fe76d322d16d178f41
Marco Nie <github@niekun.net>
2022-04-11 15:04:19 +0800

    test

--- vid_utils.py
+++ vid_utils.py
@@ -78,7 +78,7 @@ class Video:
                     raise StopIteration  # Usually the last line is empty
                 if "video only" in line:
                     continue  # I don't need video without audio
-                if "─────────────────────────────────────────────────────────────────────────────────────────────────────────────────" in line:
+                if "-------------" in line:
                     continue
             except StopIteration:
                 break
