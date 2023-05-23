74215d959b600acfa3dfc5872aa2999cc80e7536
Marco Nie <github@niekun.net>
2023-05-23 09:01:46 +0800

    update

--- sitealias.py
+++ sitealias.py
@@ -1,5 +1,5 @@
 site = {
     'youtube': 'https://www.youtube.com/watch?v=',
-    'twitter': 'https://twitter.com/BleacherReport/status/',
+    'twitter': 'https://twitter.com/i/status/',
     'pornhub': 'https://www.pornhub.com/view_video.php?viewkey='
 }
